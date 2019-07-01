7d4e607d76719a2d0a1201f0e9ff4922f069a011
maximtrp <maximtrp@gmail.com>
2019-07-01 23:55:18 +0300

    upd

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ranger-cmus
-Ranger plugin for integration with cmus audio player
+
+This plugin adds 3 custom commands for integrating [ranger](https://ranger.github.io) and [cmus](https://cmus.github.io) audio player:
+
+* `cmus_play`: sending files/folders to playlist.
+* `cmus_lib`: adding files/folders to library.
+* `cmus_queue`: enqueuing files/folders.
+
+## Installation
+
+Copy `cmus.py` file to `~/.config/ranger/plugins` folder and restart ranger.
+
+## Usage
+
+Just select or mark some files/folders and enter one of the commands, e.g. `:cmus_play`.
---
+++ cmus.py
@@ -0,0 +1,44 @@
+import os
+from ranger.api.commands import *
+from ranger.core.loader import CommandLoader
+
+def get_files(fm):
+    ftd = fm.thisdir
+    selected_files = ftd.get_selection()
+    active_file = fm.thisfile if not selected_files else None
+
+    if not selected_files and not active_file:
+        return
+    else:
+        file_objs = selected_files if selected_files else [active_file]
+    return file_objs
+
+class cmus_play(Command):
+    def execute(self):
+        """ Add selected files or folders to playlist """
+        file_objs = get_files(self.fm)
+       
+        cmus = ["cmus-remote"]
+        cmus.extend([f.path for f in file_objs])
+        self.fm.execute_command(cmus)
+        self.fm.notify("Files were sent to cmus!")
+
+class cmus_queue(Command):
+    def execute(self):
+        """ Add selected files or folders to queue """
+
+        file_objs = get_files(self.fm)
+        cmus = ["cmus-remote", "-q"]
+        cmus.extend([f.path for f in file_objs])
+        self.fm.execute_command(cmus)
+        self.fm.notify("Files were sent to cmus queue")
+
+class cmus_lib(Command):
+    def execute(self):
+        """ Add selected files or folders to library """
+
+        file_objs = get_files(self.fm)
+        cmus = ["cmus-remote", "-l"]
+        cmus.extend([f.path for f in file_objs])
+        self.fm.execute_command(cmus)
+        self.fm.notify("Files were sent to cmus library")
